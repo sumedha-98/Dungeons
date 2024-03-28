9bfcc070928a694e7af62397e0321b03eab6ac78
sumedha-98 <sumedhagat@gmail.com>
2024-03-28 13:28:51 -0700

    comments added to code

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 class DungeonGenerator:
     def __init__(self, width, height, num_rooms, max_room_width, min_room_width, max_room_height, min_room_height, overlap):
-        random.seed(10)
+        random.seed(10) #introduces psuedo-randomness
         self.width = width
         self.height = height
         self.num_rooms = num_rooms
@@ -27,10 +27,10 @@ class DungeonGenerator:
         rooms = []
         frames = []  # List to store frames
         for _ in range(self.num_rooms):
-            room_width = random.randint(self.min_room_width, self.max_room_width)  # Random room width between 5 and 15
-            room_height = random.randint(self.min_room_height, self.max_room_height)  # Random room height between 5 and 10
+            room_width = random.randint(self.min_room_width, self.max_room_width)  # Random room width between min room width and max_room_width
+            room_height = random.randint(self.min_room_height, self.max_room_height)  # Random room height between min room height and max room height
             x = random.randint(0, self.width - room_width)  # Generate random x-coordinate
-            y = random.randint(2, self.height - room_height)  # Generate random y-coordinate
+            y = random.randint(0, self.height - room_height)  # Generate random y-coordinate
             # Check if room overlaps with existing rooms
             if self.overlap == "no":
                 if any(room_overlap(x, y, room_width, room_height, rx, ry, rw, rh) for rx, ry, rw, rh in rooms):
@@ -55,11 +55,11 @@ def process_input():
     width = int(request.form['user_input_width'])  # Width of the dungeon grid
     height = int(request.form['user_input_height'])  # Height of the dungeon grid
     num_rooms = int(request.form['user_input_num_rooms'])  # Number of rooms to generate
-    max_room_width = int(request.form['user_input_max_width'])
-    min_room_width = int(request.form['user_input_min_width'])
-    max_room_height = int(request.form['user_input_max_height'])
-    min_room_height = int(request.form['user_input_min_height'])
-    overlap = request.form['yes_no']
+    max_room_width = int(request.form['user_input_max_width']) #maximum room width
+    min_room_width = int(request.form['user_input_min_width']) #minimum room width
+    max_room_height = int(request.form['user_input_max_height']) #maximum room height
+    min_room_height = int(request.form['user_input_min_height']) #minimum room height
+    overlap = request.form['yes_no'] #requirement of room overlap
 
     try:
         if max_room_width > width:
